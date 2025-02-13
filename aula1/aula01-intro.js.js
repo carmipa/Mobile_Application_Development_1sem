93cb67a6f5f6dbd08f40eb8e2a245759e7d248d1
carmipa <pabcarminati@gmail.com>
2025-02-12 22:16:09 -0300

    aula 1

---
+++ aula1/aula01-intro.js.js
@@ -0,0 +1,54 @@
+// function mensagem(){
+//     console.log("Olá, mundo!");
+// }
+
+// mensagem()
+
+// function mensagem(nome){
+//     console.log("Olá, " + nome + "!");
+// }
+// mensagem("Paulo")
+
+// function soma(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(soma(2, 3));
+
+
+// (function(){
+//     console.log("Sou uma função anonima");
+// })();
+
+
+// sem a arrow function
+// const multi = function(num1, num2){
+//     return num1 * num2;
+// }
+// let resultado = multi(4,5)
+// console.log("resultado da multiplicação: " + resultado)
+
+// // Arrow function 1
+// const multiv2 = (num1, num2) =>{
+//     return num1 * num2;
+// }
+
+// // Arrow function 2
+// const multiV3 = (num1, num2) => num1 * num2;
+
+
+// funções com parametros rest (não sei quantos parametros vão ser passados)
+// const soma = (...numeros) =>{
+//     let auxiliar = 0
+//     for(i of numeros){
+//         auxiliar++ 
+//     }
+//     return auxiliar;
+// }
+// console.log("REST: " + soma(1,2,3,4,5,6,7,8,9,10))
+
+// sintaxe dO espalhamento
+// const numbbers = [6,7,4]
+// console.log("SPREAD:", soma (...numbers))
+
+
+
---
+++ aula1/desafio.js
@@ -0,0 +1,7 @@
+
+
+const multi = (num1, num2) => num1 * num2;
+const div = (num1, num2) => num1 / num2;
+const soma = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+
