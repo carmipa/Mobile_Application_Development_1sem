41d1593d77db80d0779af54f0156cc4de17097bd
carmipa <pabcarminati@gmail.com>
2025-03-21 15:49:46 -0300

    correcao

--- aulo8_axios_api/AulaAPI/src/Telas/TelaResultado.js
+++ aulo8_axios_api/AulaAPI/src/Telas/TelaResultado.js
@@ -1,90 +1,62 @@
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, TextInput, View, ImageBackground } from 'react-native';
+import { Ionicons } from 'react-native-vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import axios from 'axios';
+import { Image } from 'expo-image';
+import API_KEY from '../API_KEY.env';
 
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  TextInput,
-    Image,
-  View}
-  from 'react-native';
-import { ImageBackground} from "react-native";
-import { Ionicons} from "react-native-vector-icons";
-import { SafeAreaView} from "react-native-safe-area-context";
-import { useState } from "react";
-import axios from "axios";
-import API_KEY from "../API_KEY.env"
-import FastImage from 'react-native-fast-image';
-
-export default function TelaResultado({route, navigation}) {
-  console.log(route);
-
+export default function TelaResultado({ route, navigation }) {
   const escolha = route.params.escolha;
   const link = `https://api.giphy.com/v1/${escolha}/search`;
-  const[textoPesquisa, setTextoPesquisa] = useState('');
+  const [textoPesquisa, setTextoPesquisa] = useState('');
   const [dados, setDados] = useState([]);
 
-  console.log(link);
-
   const solicitarDados = async (textoPesquisa) => {
-    try{
-      const resultado = await  axios.get(link,{
-        params:{
-          api_key:API_KEY,
+    try {
+      const resultado = await axios.get(link, {
+        params: {
+          api_key: API_KEY,
           q: textoPesquisa,
           limit: 25,
           offset: 0,
           rating: 'g',
-          lang: 'pt'
-        }
+          lang: 'pt',
+        },
       });
-      console.log(resultado.data.data);
-      setDados(resultado.data.data)
-    }catch (err) {
-        console.log(err)
+      setDados(resultado.data.data);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
+
   return (
-    <ImageBackground
-        source={require('../../assets/BG.png')}
-        style={styles.container}
-    >
-      <SafeAreaView style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Ionicons
-            name="chevron-back"
-            size={40}
-            color="white"
-            onPress={() => navigation.goBack()}
-        />
-        <TextInput
-            placeholder="Digite sua pesquisa"
-            style={styles.input}
-            autoCapitalize={'none'} // Não deixa as letras do teclado em maiúsculo
-            autoCorrect={false} // Não corrija o texto
-            value={textoPesquisa}
-            onChangeText={(value) => setTextoPesquisa(value)}
-        />
-        <Ionicons
-            name="search"
-            size={40}
-            color="white"
-            onPress={()=>solicitarDados(textoPesquisa)}
-        />
-      </SafeAreaView>
+      <ImageBackground source={require('../../assets/BG.png')} style={styles.container}>
+        <SafeAreaView style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Ionicons name="chevron-back" size={40} color="white" onPress={() => navigation.goBack()} />
+          <TextInput
+              placeholder="Digite sua pesquisa"
+              style={styles.input}
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={textoPesquisa}
+              onChangeText={(value) => setTextoPesquisa(value)}
+          />
+          <Ionicons name="search" size={40} color="white" onPress={() => solicitarDados(textoPesquisa)} />
+        </SafeAreaView>
 
-      <FlatList
-          data={dados}
-          renderItem={({item})=>{
-        return(
-            <Image
-              style={{width: 250, height: 250}}
-              source={{uri: item.images.original.url}}
-              resizeMode={FastImage.resizeMode.contain}
-            />
-          )
-        }}
-      />
+        <FlatList
+            data={dados}
+            renderItem={({ item }) => (
+                <Image
+                    style={{ width: 250, height: 250 }}
+                    source={{ uri: item.images.original.url }}
+                    contentFit="contain"
+                />
 
-    </ImageBackground>
+            )}
+        />
+      </ImageBackground>
   );
 }
 
@@ -92,11 +64,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-
   },
-  input:{
+  input: {
     flex: 1,
     backgroundColor: 'white',
     borderRadius: 10,
-  }
+  },
 });
